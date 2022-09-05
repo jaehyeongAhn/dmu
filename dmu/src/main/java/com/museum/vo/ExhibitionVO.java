3d280a266ab765f92b5ab8319316a677159e3f58
mw644 <mw644@192.168.219.105>
2022-09-05 17:02:46 +0900

    올려요?

---
+++ dmu/src/main/java/com/museum/vo/ExhibitionVO.java
@@ -0,0 +1,70 @@
+package com.museum.vo;
+
+public class ExhibitionVO {
+	
+	int rno;
+	String did, dexhibtion,  dstart, dend, dprice, dPlace, dinformation,dtimed;
+	
+	public int getRno() {
+		return rno;
+	}
+	public void setRno(int rno) {
+		this.rno = rno;
+	}
+	public String getDid() {
+		return did;
+	}
+	public void setDid(String did) {
+		this.did = did;
+	}
+	public String getDexhibtion() {
+		return dexhibtion;
+	}
+	public void setDexhibtion(String dexhibtion) {
+		this.dexhibtion = dexhibtion;
+	}
+	public String getDstart() {
+		return dstart;
+	}
+	public void setDstart(String dstart) {
+		this.dstart = dstart;
+	}
+	public String getDend() {
+		return dend;
+	}
+	public void setDend(String dend) {
+		this.dend = dend;
+	}
+	public String getDprice() {
+		return dprice;
+	}
+	public void setDprice(String dprice) {
+		this.dprice = dprice;
+	}
+	public String getdPlace() {
+		return dPlace;
+	}
+	public void setdPlace(String dPlace) {
+		this.dPlace = dPlace;
+	}
+	public String getDinformation() {
+		return dinformation;
+	}
+	public void setDinformation(String dinformation) {
+		this.dinformation = dinformation;
+	}
+	public String getDtimed() {
+		return dtimed;
+	}
+	public void setDtimed(String dtimed) {
+		this.dtimed = dtimed;
+	}
+	 
+	
+
+	 
+ 
+}
+
+
+
